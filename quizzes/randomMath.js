05afe9079449f94eb06db8b3326d00d360835f19
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-26 15:45:02 -0700

    init: quiz one about random math

---
+++ quizzes/randomMath.js
@@ -0,0 +1,7 @@
+/*
+ * Instructions from your teacher:
+ * Write a function that returns an integer between 
+ * 0 and 1 less than a supplied integer. 
+ * 
+ * For example: random(6) would return a number between 0  and 5. 
+*/
