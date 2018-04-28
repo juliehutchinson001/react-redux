4f002281fa4110c8007afb9c23766fb215691f37
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-04-27 22:04:36 -0700

    init: instruction to solve random array quiz

---
+++ quizzes/randomArray.js
@@ -0,0 +1,7 @@
+/*
+ * 1) The code sample provides an array fruit. 
+ * Your job is write a function that returns a 
+ * random fruit from the array. The function is 
+ * outlined but you need to fill in the body 
+ * and provide a return value. 
+ */
