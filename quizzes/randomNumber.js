69ce8a5b635ec72ee9dac538539d3db3772ed622
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-05-02 18:00:50 -0700

    feat: quiz solution to randomize a decimal number

--- quizzes/randomNumber.js
+++ quizzes/randomNumber.js
@@ -2,3 +2,11 @@
  * Write a random number (decimal) whose decimal point
  * is in the 10th point.
 */
+
+// What is the ouput? 
+for (var i = 0; i < 10; i += (0.1)) {
+    console.log("$"+i.toFixed(1))
+    console.log(Number(i.toFixed(1)))
+    console.log(Math.round(i * 100) / 100);
+    
+  } 
