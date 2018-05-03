05f6b15c570616472e41859d4bcc6aaa36baeb0c
Julie Hutchinson <julie.hutchinson001@outlook.com>
2018-05-02 17:59:35 -0700

    init: quiz instructions to randomize chars of a string

---
+++ quizzes/randomString.js
@@ -0,0 +1,36 @@
+/*
+ * Question 1
+ * 
+ * 1) Take a look at the code sample. Predict 
+ * the results, what do you think will appear 
+ * in the console when you run the code snippet? 
+ * 
+ * 2) Run the code and compare your predictions 
+ *    with the results. If the results were 
+ *    different from what you predicted look 
+ *    closely at the code and reconcile what 
+ *    made the difference. 
+ * 
+ * 3) Your job is to write a function that 
+ *    returns a random letter from a given 
+ *    string. 
+ * 
+ * 4) Functions that produce random results are 
+ *    difficult to test. Write a for loop that 
+ *    calls the function 100 times and displays 
+ *    the results. Run your code and check that 
+ *    all the letters appear in the output. 
+ * 
+ * Stretch Challenge: Write a function that examines 
+ * the results to check that all letters appear at 
+ * least once.
+ */
+
+// Question 1
+
+var letters = "abcdefghijklmnopqrstuvwxyz";
+
+console.log("The first letter is: "+letters[0]);
+console.log("The last letter is: "+letters[letters.length - 1]);
+
+
